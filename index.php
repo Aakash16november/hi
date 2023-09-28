34b5db0191a49539fdb9e8164f27a3be8e4783d0
Aakash16november <122398189+Aakash16november@users.noreply.github.com>
2023-09-28 17:00:27 +0530

    index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Food Review Website</title>
+  <style>
+    body {
+      font-family: Arial, sans-serif;
+      padding: 20px;
+    }
+
+    a {
+      font-size: 24px;
+      text-decoration: none;
+      padding: 10px;
+      background-color: #4CAF50;
+      color: white;
+      border-radius: 5px;
+    }
+
+    a:hover {
+      background-color: #45a049;
+    }
+  </style>
+</head>
+<body>
+  <a href="days.php">Food</a>
+</body>
+</html>
